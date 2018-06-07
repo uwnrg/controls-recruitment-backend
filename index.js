24f53f1a7099594c54d50cfadcd5414e395ca5c0
Ding Bei Ni Fang <dbnfang@uwaterloo.ca>
2018-06-06 22:21:15 -0400

    added post

--- index.js
+++ index.js
@@ -1,95 +1,141 @@
 const express = require('express');
 const path = require('path');
-const PORT = process.env.PORT || 5000;
 var newpage = require('./newpage');
+var bodyParser = require('body-parser');
+var app = express();
 
+const PORT = process.env.PORT || 5000;
 // const applicantModule = require('./applicant');
 // const Applicant = applicantModule.Applicant;
 // console.log(applicantModule);
 
 /*********************************************
-				Class Defintions
+                Class Defintions
 **********************************************/
-class Applicant {
+class Question {
     constructor() {        
     }
-    setQuestionResponse(fullName, email, yearAndProgram = "", favoriteLanguage = "", codingExperience = "", codingQuestions = {}) {
-        this.fullName = fullName;
-        this.email = email;
-        this.yearAndProgram = yearAndProgram;
-        this.favoriteLanguage = favoriteLanguage;
-        this.codingExperience = codingExperience;
-        this.codingQuestions = codingQuestions; 
+    setQuestionResponse(language = "", response = "") {
+        this.Language = language;
+        this.Response = response;
     }
 }
+
+class Applicant {
+    constructor() { 
+        var ShapeQuestion = new Question();
+        var ControlQuestion = new Question();
+        var RepairManQuestion = new Question();       
+    }
+    setResponse(fullName, email, program = "", year = "", 
+        favoriteLanguage = "", hardestPartCoding = "", codingExperience = "", 
+        shapeQuestion = {}, controlQuestion = {}, repairManQuestion = {}) {
+        
+        this.fullName = fullName;
+        this.email = email;
+        this.program = program;
+        this.year = year;
+
+        this.favoriteLanguage = favoriteLanguage;
+        this.hardestPartCoding = hardestPartCoding;
+        this.codingExperience = codingExperience;
+
+        this.shapeQuestion = shapeQuestion;
+        this.controlQuestion = controlQuestion;
+        this.repairManQuestion = repairManQuestion;
+        //this.codingQuestions = codingQuestions;     //codingQuestions doesn't save to database if blank
+    }
+}
+
 /*********************************************
-				Firebase setup
+                Firebase setup
 **********************************************/
 var admin = require('firebase-admin');
-var serviceAccount = require('./firebase/serviceaccountkey.json');
+var serviceAccount = require('./firebase/service-account-key.json');
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: 'https://nrg-controls-recruitment.firebaseio.com'
 });
+
 // Make reference to database
 var db = admin.database();
 var ref = db.ref();
-// Create children
+
 var applicantRef = ref.child("applicants");
+var applicant = new Applicant();
+/*********************************************
+                HTTP Requests
+**********************************************/
+app
+    .use(express.static(path.join(__dirname, 'public')))
+    .use(bodyParser.json()) // support json encoded bodies
+    .use(bodyParser.urlencoded({ extended: true })) // support encoded bodies
 
-var applicant4 = new Applicant();
-applicant4 = {
-    fullName: "Jeff Niu",
-    email: "jeffniu22@gmail.com",
-    yearAndProgram: "2B mechatronics",
-    favoriteLanguage: "questionA",
-    hardestPartCoding: "questionB",
-    codingExperience: "questionC",
+    .set('views', path.join(__dirname, 'views'))
+    .set('view engine', 'ejs')
+    .get('/', (req, res) => res.render('pages/index'))
 
-    codingQuestions: {
-    	shapeQuestion: {
-    		response: "question1",
-    		language: "c_cpp"
-    	},
-    	controlQuestion: {
-    		response: "question2",
-    		language: "c_cpp"    		
-    	},
-		repairManQuestion: {
-			response: "question3",
-    		language: "c_cpp"   
-		}
-    }
-};
+//Route parameter middleware (will run before the route is called)
+    .param('name', function(req, res, next, name) {
+        //next(); //continue to next function
+    })
 
-applicantRef.child("applicant4").set(applicant4);
+//Getting post parameters (accepting data from webpage)
+app.post('/application', function(req, res) {
+    var user_id = req.body.id;
+
+    // var fullName = req.body.fullName;
+    // var email = req.body.email;
+    applicant.setReponse(
+        req.body.fullName,
+        req.body.email,
+        req.body.program,
+        req.body.year,
+        req.body.favoriteLanguage,
+        req.body.hardestPartCoding,
+        req.body.codingExperience,
+        req.body.shapeQuestion,
+        req.body.controlQuestion,
+        req.body.repairManQuestion   
+    )
+    // applicant.fullName = "bob";
+    // applicant.email = "asdfhjkl@gmail.com";
+    // applicant.program = req.body.program;
+    // applicant.year = req.body.year;
+
+    // applicant.favoriteLanguage = req.body.favoriteLanguage;
+    // applicant.hardestPartCoding = req.body.hardestPartCoding;
+    // applicant.codingExperience = req.body.codingExperience;
+
+    // applicant.shapeQuestion = req.body.shapeQuestion; 
+    // applicant.controlQuestion = req.body.controlQuestion; 
+    // applicant.repairManQuestion = req.body.repairManQuestion; 
+
+    console.log("Application received: " + fullName + ' ' + email + ' ' + program + ' ' + year);
+});
+
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
 
 /*********************************************
-				HTTP Requests
+               Write to database
 **********************************************/
-express()
-	.use(express.static(path.join(__dirname, 'public')))
-	.set('views', path.join(__dirname, 'views'))
-	.set('view engine', 'ejs')
-	.get('/', (req, res) => res.render('pages/index'))
-	.get('/hello', (req, res) => res.send('Hello World!'))
-	.get('/cool', (req, res) => res.send(cool()))
+// // Create a new applicant
+// var applicant = new Applicant();
+// //applicant.setResponse("bob", "asdkfjh@adf.com");
+// applicant.setReponse(
+//     fullName,
+//     email,
+//     program,
+//     year,
+//     favoriteLanguage,
+//     hardestPartCoding,
+//     codingExperience,
+//     shapeQuestion,
+//     controlQuestion,
+//     repairManQuestion   
+//     );
 
-//Get parameters from URL
-	.get('/users/:userId', function (req, res) {
-		var id = req.params.userId
-		res.send(id)
-	})
-	.get('/world', function (req, res) {
-		res.send('hello world')
-	})
+applicantRef.child(applicant.fullName).set(applicant);
 
-//Connecting the contents in the newpage.js file to index.js
-	.use('/newpage', newpage)
-
-	.post('/post', function (req, res) {
-		res.send('POST request to the homepage');
-	})
-
-	.listen(PORT, () => console.log(`Listening on ${ PORT }`))
